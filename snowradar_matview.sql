28ef0749a93489d3845c85d6417695ddce83cd95
Anthony Arendt <arendta@uw.edu>
2016-02-08 15:04:17 -0800

    add snowradar materialized view

---
+++ snowradar_matview.sql
@@ -0,0 +1,6 @@
+﻿-- MATERIALIZED VIEW to link the collection name back to the snow radar points
+-- because doing a spatial join each time was way too slow
+
+CREATE MATERIALIZED VIEW snowradar_query AS
+SELECT s.elev, s.swe, l.collection FROM snowradar AS s, snowradar_lines AS l
+WHERE ST_Intersects(l.geom, s.geom);
